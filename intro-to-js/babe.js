f753dd21060f2542d8cebec0ccc76406f3d425ee
Pichaipusit Kamsorn <Pichaipusit.ks@gmail.com>
2022-02-18 13:59:31 +0700

    vein eei

--- intro-to-js/babe.js
+++ intro-to-js/babe.js
@@ -1,5 +1,5 @@
 function askMyName() {
-  const name = prompt(); // เรียกใช้คำสั่ง prompt เพื่อถามชื่อ https://www.w3schools.com/jsref/met_win_prompt.asp
+  const name = prompt("sdsd, sds"); // เรียกใช้คำสั่ง prompt เพื่อถามชื่อ https://www.w3schools.com/jsref/met_win_prompt.asp
 
   if (name != null) {
     document.getElementById("title").innerHTML = "My name is " + name;
@@ -7,6 +7,33 @@ function askMyName() {
 }
 
 function helloWorld() {
-  const h1message = document.getElementById("title").textContent;
-  alert(h1message + "ใส่คำคมหลังรถสิบล้อ" + "ขี้เกียจอ่ะะ!!!!"); // ใช้คำสั่ง document.getElementById เหมือนใน askMyName เพื่อเอาข้อความใน h1 มาใส่
+  const h1messag = document.getElementById("title").textContent;
+  alert(h1messag + "ใส่คำคมหลังรถสิบล้อ" + "ขี้เกียจอ่ะะ!!!!"); // ใช้คำสั่ง document.getElementById เหมือนใน askMyName เพื่อเอาข้อความใน h1 มาใส่
+}
+
+// Exercise 1
+let year = new Date().getFullYear();
+
+function calAge() {
+  let userAge = prompt("Enter your age:");
+  let userAgeParsed = parseInt(userAge);
+  if (Number.isInteger(userAgeParsed)) {
+    let birthYear = year - userAgeParsed;
+    alert(`ยินดีด้วย นายอยู่ในประเทศนี้มา ${birthYear} ปีแล้ว`);
+  } else {
+    alert("it's not a number! You F...");
+  }
+}
+
+// Exercise 2
+let myName = document.getElementById("my-name");
+let myProvince = document.getElementById("my-province");
+
+function loadProfile() {
+  const name = "Babe";
+  const province = "Udon Thani";
+  myName.textContent = name;
+  myProvince.textContent = province;
+  console.log("🟣");
+  console.log(myName);
 }
